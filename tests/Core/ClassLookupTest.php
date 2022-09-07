e4792d461a6676a4ff341d3297b5d9b9e5a7d577
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-09-07 11:38:53 +0200

    refactor: add ClassLookup for dynamically creating instances
    refs conjoon/php-lib-conjoon#8

---
+++ tests/Core/ClassLookupTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * conjoon
+ * php-lib-conjoon
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Conjoon\Core;
+
+use Conjoon\Core\ClassLookup;
+use Conjoon\Core\Exception\ClassNotFoundException;
+use Conjoon\Core\Exception\InvalidTypeException;
+use Tests\TestCase;
+
+/**
+ * Tests ClassLookup
+ */
+class ClassLookupTest extends TestCase
+{
+    /**
+     * tests load() with ClassNotFoundException
+     */
+    public function testGetResourceTargetWithClassNotFoundException()
+    {
+        $this->expectException(ClassNotFoundException::class);
+
+        $lookup = new ClassLookup();
+
+        $lookup->load("RandomClass", "RandomParentClass");
+    }
+
+
+    /**
+     * tests load() with InvalidTypeException
+     */
+    public function testGetResourceTargetWithInvalidTypeException()
+    {
+        $this->expectException(InvalidTypeException::class);
+
+        $lookup = new ClassLookup();
+
+        $lookup->load("Tests\\Conjoon\\JsonApi\\Resource\\TestResourceStd", "Conjoon\\Core");
+    }
+
+
+    /**
+     * tests getResourceTarget()
+     */
+    public function testGetResourceTarget()
+    {
+        $lookup = new ClassLookup();
+
+        $inst = $lookup->load(
+            "Tests\\Conjoon\\JsonApi\\Resource\\TestResourceObjectDescription",
+            "Conjoon\\JsonApi\\Resource\\ObjectDescription"
+        );
+
+        $this->assertInstanceOf(
+            "Tests\\Conjoon\\JsonApi\\Resource\\TestResourceObjectDescription",
+            $inst
+        );
+    }
+}
