cd107b562a2289966592e806d14aa556072ab792
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-09-15 12:09:23 +0200

    refactor: add SortInfo for DataManagement
    refs conjoon/php-lib-conjoon#8

---
+++ src/DataManagement/SortInfo.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * conjoon
+ * php-lib-conjoon
+ * Copyright (C) 2019-2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Conjoon\DataManagement;
+
+use Conjoon\Core\Contract\Arrayable;
+
+/**
+ * Represents sort information.
+ *
+ * @example
+ *
+ *     $sort = new SortInfo("subject", SortDirection::ASC);
+ */
+class SortInfo implements Arrayable
+{
+    /**
+     * @var string
+     */
+    protected string $field;
+
+    /**
+     * @var SortDirection
+     */
+    protected SortDirection $direction;
+
+
+    /**
+     * Constructor.
+     *
+     * @param string $field
+     * @param SortDirection $direction
+     */
+    public function __construct(string $field, SortDirection $direction)
+    {
+        $this->field     = $field;
+        $this->direction = $direction;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+
+    /**
+     * @return SortDirection
+     */
+    public function getDirection(): SortDirection
+    {
+        return $this->direction;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray(): array
+    {
+        return [
+            "field" => $this->getField(),
+            "direction" => $this->getDirection()->value
+        ];
+    }
+}
