6d6416251f460cfa01c57c469d5347eed6622d70
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-10-13 11:21:08 +0200

    refactor: allow OperandList to be an Operand
    refs conjoon/php-lib-conjoon#8

--- src/Statement/OperandList.php
+++ src/Statement/OperandList.php
@@ -30,11 +30,13 @@ declare(strict_types=1);
 namespace Conjoon\Statement;
 
 use Conjoon\Core\Data\AbstractList;
+use Conjoon\Core\Data\JsonStrategy;
+use Conjoon\Core\Data\StringStrategy;
 
 /**
  * Represents a list of Operands.
  */
-class OperandList extends AbstractList
+class OperandList extends AbstractList implements Operand
 {
     /**
      * @inheritdoc
@@ -57,5 +59,40 @@ class OperandList extends AbstractList
         }
 
         return $res;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function toJson(JsonStrategy $strategy = null): array
+    {
+        if ($strategy) {
+            return $strategy->toJson($this);
+        }
+
+        return $this->toArray();
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getValue(): mixed
+    {
+        return $this->toArray();
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function toString(StringStrategy $stringStrategy = null): string
+    {
+        if ($stringStrategy) {
+            return $stringStrategy->toString($this);
+        }
+
+        return "[" . implode(", ", $this->toArray()) . "]";
     }
 }
