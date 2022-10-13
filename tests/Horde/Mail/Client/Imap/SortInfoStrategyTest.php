3178c64a6ab733d38b43d2a295ad16ce09cd937d
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-10-13 22:41:55 +0200

    refactor: add SortInfoStrategy
    refs conjoon/php-lib-conjoon#8

---
+++ tests/Horde/Mail/Client/Imap/SortInfoStrategyTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * conjoon
+ * php-lib-conjoon
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Conjoon\Horde\Mail\Client\Imap;
+
+use Conjoon\Core\Contract\Arrayable;
+use Conjoon\Core\Data\JsonStrategy;
+use Conjoon\Core\Data\SortDirection;
+use Conjoon\Core\Data\SortInfo;
+use Conjoon\Core\Data\SortInfoList;
+use Conjoon\Horde\Mail\Client\Imap\SortInfoStrategy;
+use Horde_Imap_Client;
+use Tests\TestCase;
+
+/**
+ * Tests SortInfoStrategy.
+ */
+class SortInfoStrategyTest extends TestCase
+{
+    /**
+     * Test inheritance
+     */
+    public function testClass()
+    {
+        $strategy = new SortInfoStrategy();
+        $this->assertInstanceOf(JsonStrategy::class, $strategy);
+    }
+
+
+    /**
+     * Test toJson()
+     */
+    public function testToJson()
+    {
+        $strategy = new SortInfoStrategy();
+
+        $this->assertSame([], $strategy->toJson($this->getMockForAbstractClass(Arrayable::class)));
+
+
+        $sortInfoList = new SortInfoList();
+        $sortInfoList[] = new SortInfo("subject", SortDirection::DESC);
+        $sortInfoList[] = new SortInfo("date", SortDirection::ASC);
+        $sortInfoList[] = new SortInfo("size", SortDirection::DESC);
+
+        $strategy = new SortInfoStrategy();
+        $strategy->toJson($sortInfoList);
+
+        $result = [
+           Horde_Imap_Client::SORT_REVERSE,
+           Horde_Imap_Client::SORT_SUBJECT,
+           Horde_Imap_Client::SORT_DATE,
+           Horde_Imap_Client::SORT_REVERSE,
+           Horde_Imap_Client::SORT_SIZE
+         ];
+
+        $this->assertSame(
+            $result,
+            $strategy->toJson($sortInfoList)
+        );
+    }
+}
