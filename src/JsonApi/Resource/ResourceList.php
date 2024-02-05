1f5249b11ffa0b71a41c24fb8a17521f973cfd51
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-05 13:55:00 +0100

    refactor: remove offsetSet()- override

--- src/JsonApi/Resource/ResourceList.php
+++ src/JsonApi/Resource/ResourceList.php
@@ -1,28 +1,12 @@
 <?php
 
 /**
- * conjoon
- * php-lib-conjoon
- * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2022-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
@@ -34,54 +18,9 @@ use InvalidArgumentException;
 use OutOfBoundsException;
 use TypeError;
 
-/**
- * An abstract list maintaining entities of the type Resource.
- * This implementation makes sure that no URI duplicates may exist in a ResourceList-object.
- *
- * @extends AbstractList<Resource>
- */
+
 class ResourceList extends AbstractList
 {
-    /**
-     * @inheritdoc
-     *
-     * @throws InvalidArgumentException|OutOfBoundsException|TypeError
-     */
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        $this->assertTypeFor($value);
-
-        /**
-         * @var Resource $value
-         */
-        $value = $this->uriExists($value);
-
-        $this->doInsert($offset, $value);
-    }
-
-    /**
-     * Returns the Resource itself if its URI does not already exist in *this* ResourceList.
-     *
-     * @param Resource $resource
-     * @return Resource
-     *
-     * @throws InvalidArgumentException if the URI of the resource was already registered with
-     * this ResourceList.
-     */
-    private function uriExists(Resource $resource): Resource
-    {
-        $uri = $resource->getUri();
-
-        if (count(array_filter($this->data, fn ($res) => $res->getUri()->equals($uri))) > 0) {
-            throw new InvalidArgumentException(
-                "A resource for the URI \"" . $uri->toString() . "\" already exists"
-            );
-        }
-
-        return $resource;
-    }
-
-
     /**
      * @return string
      */
--- tests/JsonApi/Resource/ResourceListTest.php
+++ tests/JsonApi/Resource/ResourceListTest.php
@@ -1,28 +1,12 @@
 <?php
 
 /**
- * conjoon
- * php-lib-conjoon
- * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2022-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
@@ -32,14 +16,8 @@ namespace Tests\Conjoon\JsonApi\Resource;
 use Conjoon\Core\AbstractList;
 use Conjoon\JsonApi\Resource\Resource;
 use Conjoon\JsonApi\Resource\ResourceList;
-use Conjoon\Net\Uri\Component\Path\Template;
-use InvalidArgumentException;
-use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
 
-/**
- * tests ResourceList
- */
 class ResourceListTest extends TestCase
 {
     /**
@@ -51,38 +29,6 @@ class ResourceListTest extends TestCase
         $this->assertInstanceOf(AbstractList::class, $list);
 
         $this->assertSame(Resource::class, $list->getEntityType());
-    }
-
-
-    /**
-     * tests offsetSet(), makes sure no URI duplicates may occur
-     * @return void
-     */
-    public function testOffsetSet(): void
-    {
-        $resource = function (string $uri): MockObject&Resource {
-            /**
-             * @var MockObject&Resource $res
-             */
-            $res = $this->createMockForAbstract(Resource::class, ["getUri"]);
-            $res->expects($this->any())->method("getUri")->willReturn(new Template($uri));
-            return $res;
-        };
-
-        $resourceList = new ResourceList();
-        $resourceList[] = $resource("/uri1");
-        $resourceList[] = $resource("/uri2");
-        $resourceList[] = $resource("/uri3");
-
-        try {
-            $resourceList[] = $resource("/uri2");
-            $this->fail();
-        } catch (InvalidArgumentException $e) {
-            $this->assertStringContainsString(
-                strtolower("a resource for the URI \"/uri2\" already exists"),
-                strtolower($e->getMessage())
-            );
-        }
     }
 
 
