2d626323bc3e1705e386b7f95326f189351ae0c0
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-05 12:31:55 +0100

    refactor: make ResourceResolver abstract

--- src/JsonApi/Resource/ResourceResolver.php
+++ src/JsonApi/Resource/ResourceResolver.php
@@ -1,98 +1,46 @@
 <?php
 
 /**
- * conjoon
- * php-lib-conjoon
- * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2022-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
 
 namespace Conjoon\JsonApi\Resource;
 
-use Conjoon\Net\Uri;
+use Conjoon\Core\Contract\Jsonable;
+use Conjoon\Core\ParameterBag;
+use Conjoon\Data\Resource\Exception\NotFoundException as ResourceNotFoundException;
+use Conjoon\Data\Resource\Exception\RepositoryException;
+use Conjoon\Data\Resource\ResourceDescription;
+use Conjoon\Net\Uri\Component\Path\ParameterList;
 
-/**
- * Provides functionality for resolving URIs to an actual resource.
- *
- * @example
- *
- *    class EmployeeResource implements Resource {
- *        public function getUri(): Template
- *        {
- *            return new Template("/directory/employees/{id}");
- *        }
- *    }
- *
- *    class EmployeeResourceCollection extends EmployeeResource implements ResourceCollection
- *    {
- *        public function getUri(): Template
- *        {
- *            return new Template("/directory/employees");
- *        }
- *    }
- *
- *    $resources = [
- *       new EmployeeResource(),
- *       new EmployeeResourceCollection()
- *   ];
- *
- *   $resolver = new ResourceResolver(ResourceList::make(...$resources));
- *   $resolver->resolve(Uri::make("https://localhost:8080/directory/employees/1")); // EmployeeResource
- *   $resolver->resolve(Uri::make("https://localhost:8080/directory/employees")); // EmployeeResourceCollection
- *   $resolver->resolve(Uri::make("https://localhost:8080/directory/shipments")); // null
- *
- */
-class ResourceResolver
-{
-    /**
-     * @var ResourceList
-     */
-    private ResourceList $locations;
+
+abstract class ResourceResolver {
 
 
     /**
-     * @param ResourceList $locations
-     */
-    public function __construct(ResourceList $locations)
-    {
-        $this->locations = $locations;
-    }
-
-
-    /**
-     * Resolves the specified URI and returns the Resource that is located there.
-     * Returns null if the URI did not target a registered resource with this ResourceResolver.
+     * Resolves the specified $resourceDescription to a Jsonable object. Further
+     * details about the resource to resolve are provided with the specified $pathParameters
+     * and the specified $parameterBag.
      *
-     * @param Uri $uri
-     * @return Resource|null
+     * @param ResourceDescription $resourceDescription
+     * @param ParameterList $pathParameters
+     * @param ParameterBag|null $parameterBag
+     * @return Jsonable|null
+     *
+     * @throws ResourceNotFoundException|RepositoryException
      */
-    public function resolve(Uri $uri): ?Resource
-    {
-        foreach ($this->locations as $location) {
-            if ($location->getUri()->match($uri) !== null) {
-                return $location;
-            }
-        }
-        return null;
-    }
+    abstract public function resolve(
+        ResourceDescription $resourceDescription,
+        ParameterList $pathParameters,
+        ?ParameterBag $parameterBag
+    ): ?Jsonable;
+
 }
+
