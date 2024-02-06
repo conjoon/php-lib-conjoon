7bb242b2a1bad443e0c1715c13076484ad86940d
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-06 10:29:48 +0100

    refactor: make Uri implement \Stringable

--- tests/Net/UriTest.php
+++ tests/Net/UriTest.php
@@ -1,28 +1,12 @@
 <?php
 
 /**
- * conjoon
- * php-lib-conjoon
- * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/php-lib-conjoon
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2022-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
@@ -32,7 +16,7 @@ namespace Tests\Conjoon\Net;
 use BadMethodCallException;
 use Conjoon\Net\Exception\UriSyntaxException;
 use Conjoon\Net\Uri;
-use Tests\StringableTestTrait;
+use \Stringable;
 use Tests\TestCase;
 
 /**
@@ -40,8 +24,6 @@ use Tests\TestCase;
  */
 class UriTest extends TestCase
 {
-    use StringableTestTrait;
-
     public function testConstructorWithUriSyntaxException(): void
     {
         $this->expectException(UriSyntaxException::class);
@@ -84,7 +66,7 @@ class UriTest extends TestCase
 
     public function testToString(): void
     {
-        $this->runToStringTest($this->getClassToTest());
+        $this->assertInstanceOf(Stringable::class,$this->createInstanceToTest(["https://host:8080"]));
     }
 
 
